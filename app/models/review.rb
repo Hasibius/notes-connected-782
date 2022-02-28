6d3189e89445911bf5d45dcb864813c59bf609a7
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-02-28 17:44:26 +0100

    added validations in both models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :reviewer, class_name: "User"
   belongs_to :artist, class_name: "User"
+
+  validates :comment, length: { maximum: 300 }
+  validates :score, numericality: { only_integer: true }
 end
