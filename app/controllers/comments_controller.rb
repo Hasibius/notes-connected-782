e2c40c894e000a01bc9eac8bfb127a44ea0506b0
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-02-28 17:51:01 +0100

    comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,39 @@
+class CommentsController < ApplicationController
+  # CRUD except index & show
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @comment = Comment.new(comment_params)
+    @comment.event = @event
+    @comment.user = current_user
+    if @comment.save
+      redirect_to event_path(@event), notice: 'Comment was successfully created'
+    else
+      render 'events/show'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to event_path(@event), notice: 'Comment was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to event_path(@event), notice: 'Comment was successfully destroyed'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
+  end
+end
