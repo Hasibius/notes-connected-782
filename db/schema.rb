fe24f780fdbcc4b7819ee85cd1bf4db02a9b5eea
Hasib Selimovic <hasib.selimovic@gmail.com>
2022-02-28 17:58:09 +0100

    added follows model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_152046) do
+ActiveRecord::Schema.define(version: 2022_02_28_163529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2022_02_28_152046) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["genre_id"], name: "index_events_on_genre_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_follows_on_artist_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -48,4 +57,6 @@ ActiveRecord::Schema.define(version: 2022_02_28_152046) do
   end
 
   add_foreign_key "events", "genres"
+  add_foreign_key "follows", "users", column: "artist_id"
+  add_foreign_key "follows", "users", column: "follower_id"
 end
