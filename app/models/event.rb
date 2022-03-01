a9417a6496dd6c13fe176fe12625ee7d933a9091
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-03-01 11:59:22 +0100

    validations 1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :genre
+
+  validates :title, length: { minimum: 4, maximum: 100 }, presence: true
+  validates :date, presence: true
+  validates :details, presence: true, length: { maximum: 300 }
 end
