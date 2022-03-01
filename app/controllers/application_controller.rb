fc777b8d4b71f6a73911abec1a270ead5b69f7b2
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-03-01 12:41:41 +0100

    big fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,13 @@ class ApplicationController < ActionController::Base
   # line to your controller
   # skip_before_action :authenticate_user!, only: :METHOD_NAME
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name user_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name user_name bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name user_name bio])
   end
 end
