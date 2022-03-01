d9027c94e220db14bce19ccaae77ed3dfc1377a7
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-03-01 11:09:26 +0100

    comment length validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 2, maximum: 100 }
 end
