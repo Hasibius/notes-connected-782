370da76d5ec50101beea974bef99877885d1e852
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-03-01 11:47:24 +0100

    authentication for all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Protect all routes by default
+  # If you want to skip login for some pages add the following
+  # line to your controller
+  # skip_before_action :authenticate_user!, only: :METHOD_NAME
+  before_action :authenticate_user!
 end
