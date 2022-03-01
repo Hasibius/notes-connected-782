ef6a1e912a4ff1e66a0131e0deb135d3a100e643
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-03-01 15:11:08 +0100

    minor edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ApplicationRecord
 
   validates :title, length: { minimum: 4, maximum: 100 }, presence: true
   validates :date, presence: true
-  validates :details, presence: true, length: { maximum: 300 }
+  validates :details, presence: true, length: { minimum: 10, maximum: 300 }
 end
