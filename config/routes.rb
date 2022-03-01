ab880e8d29de5f38c2dfbdacef6b1e0328524d83
Hasib Selimovic <hasib.selimovic@gmail.com>
2022-03-01 18:02:37 +0100

    another day another euro

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  resources :events
   get "dashboard", to: "pages#dashboard"
   resources :attendences, only: [:show, :index]
-=======
   resources :events # needs classification after we've implemented all features
->>>>>>> master
 end
