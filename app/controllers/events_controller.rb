72dc24b842bb3e9a61e10d7743dcff5d1b0ce476
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-03-01 15:02:00 +0100

    finished the events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
-  def new; end
+  def new
+    @event = Event.new
+  end
 
   def create
     @event = Event.new(event_params)
@@ -7,6 +9,17 @@ class EventsController < ApplicationController
     @event.genre = @genre
     @event.user = current_user
     if @event.save
+      flash[:success] = "Your events was created."
+      redirect_path to events_path
     else
+      render :new
+      flash[:warning] = "ERROR: Your events was not created."
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :details)
   end
 end
