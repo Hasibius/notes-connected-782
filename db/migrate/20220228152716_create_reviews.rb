c311233a5ff93595d7a7e59a37031270203eebaa
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-03-01 11:22:44 +0100

    fix typo

--- db/migrate/20220228152716_create_reviews.rb
+++ db/migrate/20220228152716_create_reviews.rb
@@ -3,8 +3,8 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.text :comment
       t.integer :score
-      t.references :reviewer, null: false, foreign_key: { to_table: :user }
-      t.references :artist, null: false, foreign_key: { to_table: :user }
+      t.references :reviewer, null: false, foreign_key: { to_table: :users }
+      t.references :artist, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
