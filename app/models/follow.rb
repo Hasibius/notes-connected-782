cf8c2916817a9eb9d6acc352cc0222648e26ca25
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-03-02 16:54:50 +0100

    user cannot follow or attend the same twice

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :artist, class_name: "User"
+
+  validates_uniqueness_of :follower_id, scope: :artist_id, message: "cannot follow the same person twice 🤷"
 end
