5bc10d94398a9fde6fd4890739981cd0fdaff4ec
Hasib Selimovic <hasib.selimovic@gmail.com>
2022-03-02 16:23:22 +0100

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2022_03_01_150359) do
-=======
 ActiveRecord::Schema.define(version: 2022_03_02_145415) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
