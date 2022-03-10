371eacd338ed73c636fe2523578e48e896c2a1ba
Fotios Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-03-10 13:41:34 +0100

    optimized navbar controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,39 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // connect() {
-  //   const banner = document.querySelector('.banner')
-  //   if (banner) {
-  //     // set multiplier equal to banner height in vh percentage
-  //     if (window.scrollY >= (window.innerHeight * 0.6)) {
-  //       this.element.classList.add("navbar-lewagon-very-dark-blue");
-  //     } else {
-  //       this.element.classList.remove("navbar-lewagon-very-dark-blue");
-  //     }
-  //   } else {
-  //     if (window.scrollY >= (window.innerHeight * 0.2)) {
-  //       this.element.classList.add("navbar-lewagon-very-dark-blue");
-  //     } else {
-  //       this.element.classList.remove("navbar-lewagon-very-dark-blue");
-  //     }
-  //   }
-  // };
 
   updateNavbar() {
     const banner = document.querySelector('.banner')
-    if (banner) {
-      // set multiplier equal to banner height in vh percentage
-      if (window.scrollY >= (window.innerHeight * 0.6)) {
-        this.element.classList.add("navbar-lewagon-very-dark-blue");
-      } else {
-        this.element.classList.remove("navbar-lewagon-very-dark-blue");
-      }
+    const scrollPercentage = banner ? 0.6 : 0.1;
+
+    if (window.scrollY >= (window.innerHeight * scrollPercentage)) {
+      this.element.classList.add("navbar-lewagon-very-dark-blue");
     } else {
-      if (window.scrollY >= (window.innerHeight * 0.1)) {
-        this.element.classList.add("navbar-lewagon-very-dark-blue");
-      } else {
-        this.element.classList.remove("navbar-lewagon-very-dark-blue");
-      }
+      this.element.classList.remove("navbar-lewagon-very-dark-blue");
     }
   };
 }
